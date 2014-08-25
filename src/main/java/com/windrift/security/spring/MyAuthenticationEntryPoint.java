441513e88ce9c8a0effe5544ad2abc4a3b9a1781
gary@UbuntuHome <xuweigui@hotmail.com>
2014-08-25 22:26:35 +1000

    spring security

---
+++ src/main/java/com/windrift/security/spring/MyAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.windrift.security.spring;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by gary on 25/08/14.
+ */
+public class MyAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+    public MyAuthenticationEntryPoint(String loginFormUrl) {
+        super(loginFormUrl);
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        super.commence(request, response, authException);
+    }
+}
